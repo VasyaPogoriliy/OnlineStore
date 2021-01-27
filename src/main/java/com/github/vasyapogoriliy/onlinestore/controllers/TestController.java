77a20957cc679ccdaf01ae8a633e69923ff191aa
Vasya Pogoriliy <vasya7131@gmail.com>
2021-01-27 23:00:35 +0200

    updated frontend by adding js and css

--- src/main/java/com/github/vasyapogoriliy/onlinestore/controllers/TestController.java
+++ src/main/java/com/github/vasyapogoriliy/onlinestore/controllers/TestController.java
@@ -2,16 +2,40 @@ package com.github.vasyapogoriliy.onlinestore.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class TestController {
 
 
-    @GetMapping("/home")
-    public String test() {
+    //@GetMapping("/home")
+    @RequestMapping("/home")
+    public String index(Model model) {
+        model.addAttribute("title", "Home");
+        model.addAttribute("userClickHome", true);
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About Us");
+        model.addAttribute("userClickAbout", true);
+        return "index";
+    }
+
+    @RequestMapping("/services")
+    public String services(Model model) {
+        model.addAttribute("title", "Services");
+        model.addAttribute("userClickServices", true);
+        return "index";
+    }
+
+    @RequestMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("title", "Contact Us");
+        model.addAttribute("userClickContact", true);
         return "index";
     }
 
